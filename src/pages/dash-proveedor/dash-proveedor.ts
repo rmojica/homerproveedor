0b5e57bcfc932420d9fc59e643a8e96103f3e36e
= <=>
2020-10-06 15:50:44 -0600

    arreglo mala conexion del toggle

--- src/pages/dash-proveedor/dash-proveedor.ts
+++ src/pages/dash-proveedor/dash-proveedor.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Values} from '../../providers/service/values';
 import {Socket}  from 'ngx-socket-io';
+import { Observable } from 'rxjs';
 /**
  * Generated class for the DashProveedorPage page.
  *
@@ -18,10 +19,21 @@ export class DashProveedorPage {
   homerProviders = [];
 
   constructor(private socket: Socket, public navCtrl: NavController, public navParams: NavParams, public values: Values) {
+    // this.GetIfActiveUser().subscribe(data => {
+    //   console.log(data)
+    //   this.homerProviders.push(data);
+    //     for(let provider of this.homerProviders){
+    //       if(this.values.customerId == provider.id){
+    //         this.values.isActive = true;
+    //       }else{
+    //         this.values.isActive = false;
+    //       }
+    //     }
+    // })
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DashProveedorPage');
+    
   }
 
   ngOnInit() {
@@ -40,8 +52,27 @@ export class DashProveedorPage {
     //   // this.messages.push(message);
     // });
   }
+
+  // ionViewWillEnter(){
+  //   console.log("conectando al socket");
+    
+  //   this.socket.connect();
+  // }
+
+  GetIfActiveUser(){
+    let observable = new Observable(observer => {
+      this.socket.fromEvent('adduser').subscribe((data:any) => {
+        console.log('dentro den getIfActiveUser',data)
+        observer.next(data);
+      });
+    });
+    console.log('observable', observable)
+    return observable;
+  }
+
   changeToggle(){
-    if(!this.values.isActive){
+    console.log(this.values.isActive)
+    if(this.values.isActive){
         this.socket.connect();
         this.socket.emit('adduser',{id:this.values.customerId});
         this.values.isActive = true;
