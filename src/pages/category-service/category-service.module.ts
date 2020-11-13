762bdb7210c891608ef4e73ec8ce3b2f4689d544
ronald mojicaa <rmojica@numu.digital>
2020-11-12 21:10:41 -0600

    comment into new files

--- src/pages/category-service/category-service.module.ts
+++ src/pages/category-service/category-service.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { CategoryServicePage } from './category-service';
+// import { NgModule } from '@angular/core';
+// import { IonicPageModule } from 'ionic-angular';
+// import { CategoryServicePage } from './category-service';
 
-@NgModule({
-  declarations: [
-    CategoryServicePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(CategoryServicePage),
-  ],
-})
-export class CategoryServicePageModule {}
+// @NgModule({
+//   declarations: [
+//     CategoryServicePage,
+//   ],
+//   imports: [
+//     IonicPageModule.forChild(CategoryServicePage),
+//   ],
+// })
+// export class CategoryServicePageModule {}
